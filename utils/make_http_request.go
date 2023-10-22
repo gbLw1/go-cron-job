779fece159d01b8fce4d62dcb15b519cb42db68e
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-22 00:06:12 -0300

    .

--- utils/make_http_request.go
+++ utils/make_http_request.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	"encoding/json"
-  "fmt"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -10,57 +10,56 @@ import (
 )
 
 func MakeHTTPRequest[T any](fullUrl, httpMethod string, headers map[string]string, queryParameters url.Values, body io.Reader, responseType T) (T, error) {
-  client := http.Client{}
-  u, err := url.Parse(fullUrl)
-  if err != nil {
-    return responseType, err
-  }
+	client := http.Client{}
+	u, err := url.Parse(fullUrl)
+	if err != nil {
+		return responseType, err
+	}
 
-  if httpMethod == "GET" {
-    q := u.Query()
+	if httpMethod == "GET" {
+		q := u.Query()
 
-    for k,v := range queryParameters {
-      q.Set(k, strings.Join(v, ","))
-    }
+		for k, v := range queryParameters {
+			q.Set(k, strings.Join(v, ","))
+		}
 
-    u.RawQuery = q.Encode()
-  }
+		u.RawQuery = q.Encode()
+	}
 
-  req, err := http.NewRequest(httpMethod, u.String(), body)
-  if err != nil {
-    return responseType, err
-  }
+	req, err := http.NewRequest(httpMethod, u.String(), body)
+	if err != nil {
+		return responseType, err
+	}
 
-  for k,v := range headers {
-    req.Header.Set(k, v)
-  }
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
 
-  res, err := client.Do(req)
-  if err != nil {
-    return responseType, err
-  }
+	res, err := client.Do(req)
+	if err != nil {
+		return responseType, err
+	}
 
-  if res == nil {
-    return responseType, fmt.Errorf("error: calling %s returned empty response", u.String())
-  }
+	if res == nil {
+		return responseType, fmt.Errorf("error: calling %s returned empty response", u.String())
+	}
 
-  responseData, err := io.ReadAll(res.Body)
-  if err != nil {
-    return responseType, err
-  }
+	responseData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return responseType, err
+	}
 
-  defer res.Body.Close()
+	defer res.Body.Close()
 
-  if res.StatusCode != http.StatusOK {
-    return responseType, fmt.Errorf("error: calling %s:\nstatus: %s\nresponse data: %s", u.String(), res.Status, responseData)
-  }
+	if res.StatusCode != http.StatusOK {
+		return responseType, fmt.Errorf("error: calling %s:\nstatus: %s\nresponse data: %s", u.String(), res.Status, responseData)
+	}
 
-  var responseObj T
-  err = json.Unmarshal(responseData, &responseObj)
-  if err != nil {
-    return responseType, err
-  }
+	var responseObj T
+	err = json.Unmarshal(responseData, &responseObj)
+	if err != nil {
+		return responseType, err
+	}
 
-  return responseObj, nil
+	return responseObj, nil
 }
-
